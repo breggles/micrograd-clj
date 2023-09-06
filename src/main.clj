c9ae83fdeabf5963466bb2e2fe0c80b7927ba03e
breggles <tilman.bregler@red-gate.com>
2023-09-06 22:12:59 +0100

    don't use records

--- src/main.clj
+++ src/main.clj
@@ -1,20 +1,31 @@
 (ns main)
 
-(defrecord Value [data children op])
+(defn value [data]
+  {:data data})
+
+(defn bin-op [v1 v2 op]
+  {:data    (op (:data v1)
+                (:data v2))
+   :children [v1 v2]
+   :op       'op})
 
 (defn add [v1 v2]
-  {:data    (+ (:data v1)
+  (bin-op v1 v2 +))
+
+(defn mul [v1 v2]
+  {:data    (* (:data v1)
                (:data v2))
    :children [v1 v2]
-   :op       '+'})
+   :op       *})
 
 (comment
 
   (->Value 3 [] identity)
   (map->Value {:data 3})
 
-  (add (Value. 3 [] identity)
-       (Value. 2 [] identity))
+  (mul (value 4)
+       (add (value 3)
+            (value 2)))
 
   (defn f [x]
     (- (* 3 (Math/pow x 2))
