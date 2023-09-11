aca4a1e84805307aad6dcdcbd5734920654e4d2e
breggles <tilman.bregler@red-gate.com>
2023-09-11 22:34:52 +0100

    backward

--- src/main.clj
+++ src/main.clj
@@ -1,28 +1,55 @@
 (ns main)
 
-(defn value [data]
-  {:data data})
+(defn value [v]
+  {:val  v
+   :grad 0})
 
-(defn bin-op [op v1 v2]
-  {:data    (op (:data v1)
-                (:data v2))
-   :children [v1 v2]
-   :op       op})
+(defn bin-op [op a b]
+  {:val      (op (:val a) (:val b))
+   :children [a b]
+   :op       op
+   :grad     0})
 
-(defn add [v1 v2]
-  (bin-op + v1 v2))
+(defn mul-grad [old other]
+  (+ old other))
 
-(defn mul [v1 v2]
-  (bin-op * v1 v2))
+(defn add [a b]
+  (bin-op + a b))
+
+(defn mul [a b]
+  (bin-op * a b))
+
+(defn backward [expr]
+  (condp  = (:op expr)
+    nil expr
+    +   (-> expr
+            (assoc-in [:children 0 :grad] (:grad expr))
+            (assoc-in [:children 1 :grad] (:grad expr)))
+    *   (-> expr
+            (assoc-in [:children 0 :grad] (* (:grad expr) (get-in expr [:children 1 :val])))
+            (assoc-in [:children 1 :grad] (* (:grad expr) (get-in expr [:children 0 :val]))))))
 
 (comment
 
-  (->Value 3 [] identity)
-  (map->Value {:data 3})
+  (backward (assoc (mul (value 3)
+                        (value 2))
+                   :grad
+                   1))
 
-  (clojure.pprint/pprint (mul (value 4)
-       (add (value 3)
-            (value 2))))
+  (backward (assoc (add (value 3)
+                     (value 2))
+                   :grad
+                   1))
+
+  (backward (value 3))
+
+  (->Value 3 [] identity)
+  (map->Value {:val 3})
+
+  (clojure.pprint/pprint
+    (mul (value 4)
+         (add (value 3)
+              (value 2))))
 
   (defn f [x]
     (- (* 3 (Math/pow x 2))
