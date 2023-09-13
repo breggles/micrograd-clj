9fe5e9a9b6ded0fa0e24a6396063db8b4f1ee396
breggles <tilman.bregler@red-gate.com>
2023-09-13 22:08:16 +0100

    init-grad

--- src/main.clj
+++ src/main.clj
@@ -26,26 +26,45 @@
 (defn backwards [expr]
   (condp = (:op expr)
     nil expr
-    +   (-> expr
-            (assoc-in [:kids 0 :grad] (* (:grad expr) 1))
-            (assoc-in [:kids 1 :grad] (* (:grad expr) 1)))
-    *   (-> expr
-            (assoc-in [:kids 0 :grad] (* (:grad expr) (get-in expr [:kids 1 :val])))
-            (assoc-in [:kids 1 :grad] (* (:grad expr) (get-in expr [:kids 0 :val]))))
+    +         (-> expr
+                  (assoc-in [:kids 0 :grad] (* (:grad expr) 1))
+                  (assoc-in [:kids 1 :grad] (* (:grad expr) 1)))
+    *         (-> expr
+                  (assoc-in [:kids 0 :grad] (* (:grad expr) (get-in expr [:kids 1 :val])))
+                  (assoc-in [:kids 1 :grad] (* (:grad expr) (get-in expr [:kids 0 :val]))))
     math/tanh (-> expr
-                  (assoc-in [:kids 0 :grad] (* (:grad expr) (- 1 (math/pow (math/tanh (get-in expr [:kids 0 :val])) 2)))))))
+                  (assoc-in [:kids 0 :grad] (* (:grad expr) (- 1 (math/pow (:val expr) 2)))))))
+
+(defn init-grad [expr]
+  (assoc expr :grad 1))
 
 (defn propagate [expr]
   (clojure.walk/prewalk
     backwards
-    (assoc expr :grad 1)))
+    (init-grad expr)))
 
 (comment
 
-  (backwards
-    (assoc (tanh (value 0.8814))
-           :grad
-           1))
+  (def x1 (value 2))
+  (def x2 (value 0))
+  (def w1 (value -3))
+  (def w2 (value 1))
+  (def b (value 6.8813735870195432))
+  (def x1w1 (mul x1 w1))
+  (def x2w2 (mul x2 w2))
+  (def x1w1+x2w2 (add x1w1 x2w2))
+  (def n (add x1w1+x2w2 b))
+  (def o (tanh n))
+
+  (propagate o)
+
+  (def a (value 3))
+
+  (-> (add a a) propagate)
+
+  (-> (mul (value 2) (value 3)) propagate)
+
+  (-> 0.8814 value tanh init-grad backwards)
 
   (propagate (mul (value -1)
                   (add (value 3)
