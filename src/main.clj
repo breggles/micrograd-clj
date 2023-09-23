84b3c26c69f31533dd071d4ccd1407aa10dadc35
breggles <tilman.bregler@red-gate.com>
2023-09-23 11:21:59 +0100

    asdf

--- src/main.clj
+++ src/main.clj
@@ -1,18 +1,15 @@
 (ns main
   (:require [clojure.math :as math]))
 
-(defn value [v]
-  {:val  v
+(defn const [v]
+  {:val  (atom v)
    :grad (atom 0)})
 
-(defn defop [op & params]
-  {:val  (apply op (map :val params))
+(defn- defop [op & params]
+  {:val  (atom nil)
    :kids (vec params)
    :op   op
    :grad (atom 0)})
-
-(defn mul-grad [old other]
-  (+ old other))
 
 (defn add [a b]
   (defop + a b))
@@ -21,7 +18,7 @@
   (defop * a b))
 
 (defn neg [a]
-  (mul a (value -1)))
+  (mul a (const -1)))
 
 (defn sub [a b]
   (add a (neg b)))
@@ -32,7 +29,22 @@
 (defn tanh [x]
   (defop math/tanh x))
 
-(defn init-grad [expr]
+(defn forward! [expr]
+  (->> (tree-seq :kids :kids expr)
+       (distinct)
+       (filter :kids)
+       (reverse)
+       (map
+         (fn [node]
+           (reset!
+             (:val node)
+             (apply
+               (:op node)
+               (map (comp deref :val) (:kids node))))))
+       (dorun))
+  expr)
+
+(defn init-grad! [expr]
   (reset! (expr :grad) 1)
   expr)
 
@@ -43,11 +55,11 @@
   expr)
 
 (defn get-kid-val [expr kid]
-  (get-in expr [:kids kid :val]))
+  @(get-in expr [:kids kid :val]))
 
-(defn debug [x] (print x) x)
+(defn debug [x] (clojure.pprint/pprint x) x)
 
-(defn backwards! [expr]
+(defn derive-kids! [expr]
   (condp = (:op expr)
     nil       expr
     +         (-> expr
@@ -61,14 +73,15 @@
                                                    (get-kid-val expr 0))
                                                 (dec (get-kid-val expr 1)))))
     math/tanh (-> expr
-                  (update-kid-grad! 0 (- 1 (math/pow (:val expr) 2))))))
+                  (update-kid-grad! 0 (- 1 (math/pow @(:val expr) 2))))))
 
-(defn propagate! [expr]
-  (->> (init-grad expr)
+(defn backward! [expr]
+  (->> (init-grad! expr)
        (tree-seq :kids :kids)
        (distinct)
-       (map backwards!)
-       (dorun))
+       (map derive-kids!)
+       (dorun)
+       )
   expr)
 
 (defn zero! [expr]
@@ -84,8 +97,8 @@
 (defn neuron [input-count]
   {:weights (->> rand-val
                  (repeatedly input-count)
-                 (map value))
-   :bias    (value (rand-val))})
+                 (map const))
+   :bias    (const (rand-val))})
 
 (defn fire-neuron [neuron inputs]
   (->> (:weights neuron)
@@ -117,7 +130,7 @@
   (set (flatten (map layer-params perceptron))))
 
 (defn loss [targets predictions]
-  (reduce add (map (comp #(pow % (value 2)) sub) predictions targets)))
+  (reduce add (map (comp #(pow % (const 2)) sub) predictions targets)))
 
 (defn update-params [expr params]
   (clojure.walk/postwalk
@@ -131,13 +144,13 @@
 
 (comment
 
-  (def inputs (map (partial map (partial value))
+  (def inputs (map (partial map (partial const))
               [[2    3 -1  ]
                [3   -1  0.5]
                [0.5  1  1  ]
                [1    1 -1  ]]))
 
-  (def targets (map (partial value) [1 -1 -1 1]))
+  (def targets (map (partial const) [1 -1 -1 1]))
 
   (def mlp (multi-layer-perceptron 3 [4 4 1]))
 
@@ -145,7 +158,7 @@
 
   (def l (loss targets predictions))
 
-  (:val l) (:grad l) (propagate! l)
+  (:val l) (:grad l) (backward! l)
 
   ((comp first :weights first first) mlp)
 
@@ -158,31 +171,31 @@
 
   (neuron-params (neuron 3))
 
-  (def t (init-grad (value 3)))
+  (def t (init-grad! (const 3)))
 
   (zero! t)
 
-  (zero! (init-grad (value 3)))
+  (zero! (init-grad! (const 3)))
 
   (clojure.pprint/pprint
     (fire-perceptron
       (multi-layer-perceptron 1 [1])
-      [(value 2)]))
+      [(const 2)]))
 
   (clojure.pprint/pprint
-    (propagate!
+    (backward!
       (first
         (fire-perceptron
           (multi-layer-perceptron 3 [4 4 1])
-          [(value 2) (value 3) (value -1)]))))
+          [(const 2) (const 3) (const -1)]))))
 
-  (fire-layer (layer 3 4) [(value 1) (value 2) (value 3)])
+  (fire-layer (layer 3 4) [(const 1) (const 2) (const 3)])
 
-  (def x1 (value 2))
-  (def x2 (value 0))
-  (def w1 (value -3))
-  (def w2 (value 1))
-  (def b (value 6.8813735870195432))
+  (def x1 (const 2))
+  (def x2 (const 0))
+  (def w1 (const -3))
+  (def w2 (const 1))
+  (def b (const 6.8813735870195432))
   (def x1w1 (mul x1 w1))
   (def x2w2 (mul x2 w2))
   (def x1w1+x2w2 (add x1w1 x2w2))
@@ -191,16 +204,16 @@
 
   (get-in o [:kids 0 :grad])
 
-  (tree-seq :kids :kids (mul (value 4) (add (value 2) (value 3))))
+  (tree-seq :kids :kids (mul (const 4) (add (const 2) (const 3))))
 
   (clojure.pprint/pprint
-    (fire-neuron (neuron 3) [(value 1) (value 2) (value 3)]))
+    (fire-neuron (neuron 3) [(const 1) (const 2) (const 3)]))
 
-  (clojure.pprint/pprint (propagate! o))
+  (clojure.pprint/pprint (backward! o))
 
-  (def a (value 3))
+  (def a (const 3))
 
-  (-> (add a a) propagate!)
+  (-> (add a a) backward!)
 
   (def b a)
 
@@ -208,37 +221,37 @@
 
   (reset! (:grad a) 1)
 
-  (-> (mul (value 2) (value 3)) propagate!)
+  (-> (mul (const 2) (const 3)) backward!)
 
-  (-> (pow (value 2) (value 3))
-      (init-grad)
-      (backwards!))
+  (-> (pow (const 2) (const 3))
+      (init-grad!)
+      (derive-kids!))
 
-  (-> 0.8814 value tanh init-grad backwards!)
+  (-> 0.8814 const tanh init-grad! derive-kids!)
 
-  (propagate! (mul (value -1)
-                   (add (value 3)
-                        (value 2))))
+  (backward! (mul (const -1)
+                   (add (const 3)
+                        (const 2))))
 
   (clojure.walk/prewalk
-    backwards!
-    (init-grad (mul (value -1)
-                    (add (value 3)
-                         (value 2)))))
+    derive-kids!
+    (init-grad! (mul (const -1)
+                    (add (const 3)
+                         (const 2)))))
 
-  (backwards! (init-grad (mul (value 3)
-                             (value 2))))
+  (derive-kids! (init-grad! (mul (const 3)
+                             (const 2))))
 
-  (backwards! (init-grad (add (value 3)
-                             (value 2))))
+  (derive-kids! (init-grad! (add (const 3)
+                             (const 2))))
 
-  (backwards! (add (value 3) (value 2)))
+  (derive-kids! (add (const 3) (const 2)))
 
-  (backwards! (value 3))
+  (derive-kids! (const 3))
 
   (clojure.pprint/pprint
-    (mul (value 4)
-         (add (value 3)
-              (value 2))))
+    (mul (const 4)
+         (add (const 3)
+              (const 2))))
 
   )
--- test/main_test.clj
+++ test/main_test.clj
@@ -1,6 +1,66 @@
 (ns main-test
-  (:require [clojure.test :refer [deftest is testing]]))
+  (:require [clojure.test :refer [deftest is testing]]
+            [main :refer :all]))
 
 (deftest micrograd-clj-test
-  (testing "The micrograds"
-    (is (= 1 2))))
+  (testing "const"
+    (is (= 3 @(:val (const 3))))
+    (is (= 0 @(:grad (const 3)))))
+  (testing "add"
+    (is (= nil @(:val (add (const 2) (const 3)))))
+    (is (= 0 @(:grad (add (const 2) (const 3))))))
+  (testing "forward!"
+    (is (= 5 @(:val (forward! (add (const 2) (const 3))))))
+    (is (= 20 @(:val (forward! (mul (const 4) (add (const 2) (const 3))))))))
+  (testing "init-grad!"
+    (is (= 1 @(:grad (init-grad! (const 0))))))
+  (testing "derive-kids!"
+    (is (= [1 1] (->> (add (const 2) (const 3))
+                      (forward!)
+                      (init-grad!)
+                      (derive-kids!)
+                      (:kids)
+                      (map (comp deref :grad))))))
+  (testing "backward!"
+    (is (= [1 1 1] (->> (add (const 2) (const 3))
+                        (forward!)
+                        (backward!)
+                        (tree-seq :kids :kids)
+                        (map :grad)
+                        (map deref))))
+    (is (= [1 1 1] (->> (mul (const 4) (add (const 2) (const 3)))
+                        (forward!)
+                        (backward!)
+                        debug
+                        (tree-seq :kids :kids)
+                        (map :grad)
+                        (map deref)))))
+  )
+
+(comment
+
+  (def expr (mul (const 4) (add (const 2) (const 3))))
+
+  (get-in expr [:kids 1])
+
+  (clojure.walk/prewalk
+    debug
+    expr)
+
+  (def a (const 3))
+
+  (def e {:kids [a a]})
+
+  (def e (assoc-in e [:kids 0 :grad] 1))
+
+  (= (get-in e [:kids 0]) (get-in e [:kids 1]))
+
+  (reduce
+    (fn [e v]
+      (let [node (get-in e v)
+            value (apply (:op node) (map :val (:kids node)))]
+        (assoc-in e (conj v :val) value)))
+    expr
+    '([:kids 1] []))
+
+)
